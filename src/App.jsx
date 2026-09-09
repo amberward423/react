6c9ce29f9c3e7cf6bbd9eeb9687b118173a905f3
amberward423 <amberward423@gmail.com>
2026-09-09 12:10:14 +0300

    Add React starter project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Home from "./components/Home.jsx";
+
+import "./App.css";
+const App = () => {
+  return (
+    <>
+      <h1>My App</h1>
+      <Home />
+    </>
+  );
+};
+export default App;
