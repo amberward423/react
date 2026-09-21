12529b6aa47ce930fdfed9c4f7745762e4b83d58
amberward423 <amberward423@gmail.com>
2026-09-21 16:53:45 +0300

    Add media fetching with hooks

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,45 +1,9 @@
+import fetchData from "../utils/fetchData.js";
 import MediaRow from "../components/MediaRow.jsx";
-const Home = () => {
-  const mediaArray = [
-    {
-      media_id: 8,
-      user_id: 5,
-      filename:
-        "https://images.pexels.com/photos/34388974/pexels-photo-34388974.jpeg",
-      thumbnail:
-        "https://images.pexels.com/photos/34388974/pexels-photo-34388974.jpeg",
-      filesize: 170469,
-      media_type: "image/jpeg",
-      title: "Picture 1",
-      description: "Sleepy cat",
-      created_at: "2024-01-07T20:49:34.000Z",
-    },
-    {
-      media_id: 9,
-      user_id: 7,
-      filename:
-        "https://images.pexels.com/photos/18001795/pexels-photo-18001795.jpeg",
-      thumbnail:
-        "https://images.pexels.com/photos/18001795/pexels-photo-18001795.jpeg",
-      filesize: 1002912,
-      media_type: "image/jpeg",
-      title: "Picture 2",
-      description: "Adventurous cat",
-      created_at: "2024-01-07T21:32:27.000Z",
-    },
-    {
-      media_id: 17,
-      user_id: 2,
-      filename: "https://www.pexels.com/download/video/37957099/",
-      thumbnail: "https://www.pexels.com/download/video/37957099/",
-      filesize: 1236616,
-      media_type: "video/mp4",
-      title: "Picture 3",
-      description: "Kitten",
-      created_at: "2024-01-07T20:48:13.000Z",
-    },
-  ];
+import {useState} from 'react';
 
+const Home = () => {
+  const [mediaArray, setMediaArray] = useState([]);
 
   return (
     <>
