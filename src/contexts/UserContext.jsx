d10b99ceeda66be6caaf135e0c3cc689eec1348f
amberward423 <amberward423@gmail.com>
2026-09-23 13:10:58 +0300

    Add user context

---
+++ src/contexts/UserContext.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState } from "react";
+import { useAuthentication, useUser } from "../hooks/apiHooks";
+import { useNavigate } from "react-router";
+
+
+const UserContext = createContext(null);
+
+
+
+const UserProvider = {(children)} => {
+
+
+    
+}
